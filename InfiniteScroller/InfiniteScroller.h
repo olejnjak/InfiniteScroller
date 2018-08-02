56803a2422b6ee6c062aec7b22ff7c16927f5800
Jakub Olejník <olejnjak@gmail.com>
2018-08-02 14:23:01 +0200

    Initial Commit

---
+++ InfiniteScroller/InfiniteScroller.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteScroller.h
+//  InfiniteScroller
+//
+//  Created by Jakub Olejník on 02/08/2018.
+//  Copyright © 2018 Jakub Olejník. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteScroller.
+FOUNDATION_EXPORT double InfiniteScrollerVersionNumber;
+
+//! Project version string for InfiniteScroller.
+FOUNDATION_EXPORT const unsigned char InfiniteScrollerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteScroller/PublicHeader.h>
+
+
